82d0fa5a4f264aad4ec20e1f0268159f5b247e36
Tero Isoaho <tero.isoaho@kavokerr.com>
2020-06-22 23:39:56 +0300

    Working code with tests.

--- src/eke.c
+++ src/eke.c
@@ -1,8 +1,95 @@
+#include <ctype.h>
+#include <string.h>
+#include <limits.h>
 #include "eke.h"
 
+
+// swaps the given chars using a temporary variable.
+static void swap(char* a, char* b)
+{
+    char tmp;
+
+    tmp = *a;
+
+    *a = *b;
+    *b = tmp;
+}
+
+
+
+
+static void bufToLowerCase(char* buffer, size_t len) {
+    
+    for (unsigned i = 0; i < len; i++) {
+        buffer[i] = (char) tolower(buffer[i]);
+    }
+    
+}
+
+static int charsValue(char in)
+{
+    static const char order[] = "qwertyuiopåasdfghjklöäzxcvbnm";
+
+    char* pos = strchr(order, in);
+
+    if (pos == NULL) {
+        return INT_MAX; // everything else than alphas pushed to end. 
+    }
+    else {
+        return (int) (pos - order); // 0 is the first (=q) and so on....
+    }
+}
+
+
+// this implements quick sort kind of algorithm for the sorting
+static void do_sort(char* buffer, size_t len)
+{
+    if (len < 2)
+        return; // DONE!
+    
+    int pivot = charsValue(buffer[len / 2]);
+    size_t i, j;
+    
+    for (i = 0, j = len - 1; ; i++, j--) {
+        while (charsValue(buffer[i]) < pivot) {
+            i++;   
+        }
+        while (charsValue(buffer[j]) > pivot) {
+            j--;
+        }
+        
+        if (i >= j) {
+            break;
+        }
+        swap(&buffer[i], &buffer[j]);
+    }
+ 
+  do_sort(buffer, i);
+  do_sort(buffer + i, len - i);
+    
+}
+
+void fix_ending(char* buffer, size_t len)
+{
+    while (!isalpha(buffer[len])) {
+        buffer[len] = '\0';
+        len--;
+    }
+}
+
+// sorts the buffer by the specified criteria. everything is conveted to
+// lower case. Non-alphabetic characters are swallowed.
 int eke_sort(char* buffer)
 {
-    (void) buffer;
+    if (!buffer) {
+        return -1; // bad input;
+    }
+    
+    size_t len = strlen(buffer);
+
+    bufToLowerCase(buffer, len);
+    do_sort(buffer, len);
+    fix_ending(buffer, len);
     
     return 0;
 }
--- tests/eke-test.cpp
+++ tests/eke-test.cpp
@@ -11,7 +11,7 @@ extern "C" {
 TEST_GROUP(eke)
 {
     void setup()
-    {
+    { 
         
     }
 
@@ -20,20 +20,43 @@ TEST_GROUP(eke)
     }
 };
 
-TEST(eke, can_be_called)
-{
-    char test_dummy[] = "DUMMY";
-    
-    CHECK_EQUAL(0, eke_sort(test_dummy));
-}
 
 
 TEST(eke, sorts_according_to_spec)
 {
 
     char string_to_sort[] = "eke-elektroniikka";
-    const char expected[] = "eeeertiioakkkln";
+    const char expected[] = "eeeertiioakkkkln"; // REALLY! There is 4 k's!
 
     eke_sort(string_to_sort);
     STRCMP_EQUAL(expected, string_to_sort);
+}
+
+TEST(eke, consumes_non_alpha_characters)
+{
+    char string_to_sort[] = "4732568462-.'*eke-elektroniikka67146719647165-,.,.'";
+    const char expected[] = "eeeertiioakkkkln";
+
+    eke_sort(string_to_sort);
+    STRCMP_EQUAL(expected, string_to_sort);
+}
+
+TEST(eke, operates_on_longer_input)
+{
+    char string_to_sort[] = "eke-elektroniikka3463274856754q761876547164752784567823465";
+
+    CHECK_EQUAL(0, eke_sort(string_to_sort));
+}
+
+TEST(eke, copes_with_an_empty_string)
+{
+    char string_to_sort[] = "";
+
+    CHECK_EQUAL(0, eke_sort(string_to_sort));
+    
+}
+
+TEST(eke, returns_fail_with_null_string)
+{
+    CHECK_EQUAL(-1, eke_sort(NULL)); 
 }
